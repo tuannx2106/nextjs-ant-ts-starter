e3a5ee942d398fbb3c21467855cbd9c27be40f38
tuannx <xuantuan.nguyen@shopee.com>
2021-08-16 21:31:27 +0700

    update

--- next.config.js
+++ next.config.js
@@ -2,18 +2,21 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withAntdLess = require('next-plugin-antd-less');
 
-module.exports = withAntdLess({
-  // optional
-  // optional
-  lessVarsFilePath: './src/styles/variables.less',
-  // optional
-  lessVarsFilePathAppendToEndOfContent: false,
-  // optional https://github.com/webpack-contrib/css-loader#object
-  cssLoaderOptions: {},
+module.exports = {
+  ...withAntdLess({
+    // optional
+    // optional
+    lessVarsFilePath: './src/styles/variables.less',
+    // optional
+    lessVarsFilePathAppendToEndOfContent: false,
+    // optional https://github.com/webpack-contrib/css-loader#object
+    cssLoaderOptions: {},
 
-  // Other Config Here...
+    // Other Config Here...
 
-  webpack(config) {
-    return config;
-  },
-});
+    webpack(config) {
+      return config;
+    },
+  }),
+  basePath: '/test',
+};
